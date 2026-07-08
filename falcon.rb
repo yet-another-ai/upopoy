609aceef826d71e0411d48e1b8dc014a620a9546
Delton Ding <dsh0416@gmail.com>
2026-07-08 21:13:40 +0800

    ci: add phase of docker build (#1)
    * ci: add phase of docker build
    
    * fixes

--- falcon.rb
+++ falcon.rb
@@ -1,10 +1,11 @@
 #!/usr/bin/env falcon-host
 # frozen_string_literal: true
 
-require "async/service/generic"
-require "rbconfig"
-require "uri"
+require 'async/service/generic'
+require 'rbconfig'
+require 'uri'
 
+# Runs an external command as an async-service managed process.
 class CommandService < Async::Service::Generic
   def setup(container)
     super
@@ -16,41 +17,55 @@ class CommandService < Async::Service::Generic
 end
 
 root_path = __dir__
-api_url = ENV.fetch("UPOPOY_API_URL", "http://127.0.0.1:3001")
+container = ENV['UPOPOY_CONTAINER'] == 'true'
+backend_path = container ? root_path : File.join(root_path, 'backend')
+api_url = container ? 'http://127.0.0.1:9292' : ENV.fetch('UPOPOY_API_URL', 'http://127.0.0.1:3001')
 api_uri = URI(api_url)
 api_bind = "#{api_uri.scheme}://#{api_uri.host}:#{api_uri.port}"
 ruby = RbConfig.ruby
-bundle = Gem.bin_path("bundler", "bundle")
+bundle = Gem.bin_path('bundler', 'bundle')
 
-service "upopoy-api" do
+service 'upopoy-api' do
   service_class CommandService
-  directory File.join(root_path, "backend")
+  directory backend_path
   command [
     ruby,
     bundle,
-    "exec",
-    "falcon",
-    "serve",
-    "--bind",
+    'exec',
+    'falcon',
+    'serve',
+    '--bind',
     api_bind,
-    "--config",
-    "config.ru",
-    "--threaded",
-    "--count",
-    ENV.fetch("FALCON_WORKERS", "1")
+    '--config',
+    'config.ru',
+    '--threaded',
+    '--count',
+    ENV.fetch('FALCON_WORKERS', '1')
   ]
 end
 
-service "upopoy-frontend" do
-  service_class CommandService
-  directory File.join(root_path, "frontend")
-  command [
-    { "VITE_RAILS_PROXY_TARGET" => api_url },
-    "pnpm",
-    "dev",
-    "--host",
-    ENV.fetch("VITE_HOST", "0.0.0.0"),
-    "--port",
-    ENV.fetch("VITE_PORT", "3000")
-  ]
+if container
+  service 'upopoy-nginx' do
+    service_class CommandService
+    directory root_path
+    command [
+      'nginx',
+      '-g',
+      'daemon off;'
+    ]
+  end
+else
+  service 'upopoy-frontend' do
+    service_class CommandService
+    directory File.join(root_path, 'frontend')
+    command [
+      { 'VITE_RAILS_PROXY_TARGET' => api_url },
+      'pnpm',
+      'dev',
+      '--host',
+      ENV.fetch('VITE_HOST', '0.0.0.0'),
+      '--port',
+      ENV.fetch('VITE_PORT', '3000')
+    ]
+  end
 end
